5a53e41c7d3bc331fa8585e1ac0ae49400e0b361
AnhTrung1997 <trananhtrungtm@gmail.com>
2024-11-24 01:15:02 +0700

    tr

--- src/components/AllEmployees/AddEmployee/AddEmployee.jsx
+++ src/components/AllEmployees/AddEmployee/AddEmployee.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './AddEmployee.css'
 import { FaUser } from "react-icons/fa6";
 import { SlBriefcase } from "react-icons/sl";
@@ -7,8 +7,29 @@ import { IoDocumentTextOutline } from "react-icons/io5";
 import { IoAccessibilityOutline } from "react-icons/io5";
 import { IoCameraOutline } from "react-icons/io5";
 import React, { useRef } from 'react';
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { Modal4 } from '../../../Modal4';
+
 
 const AddEmployee = () => {
+    const [check, setCheck] = useState(false)
+    const [arr, setArr] = useState([])
+    const [toogle, setToogle] = useState(false)
+    const fetchOne = async () => {
+        try {
+            setCheck(true)
+            const respone = await fetch('https://671c5ff22c842d92c382ba18.mockapi.io/mindxpro')
+            const data = await respone.json()
+            setArr(data)
+            setCheck(false)
+        }
+        catch {
+            console.log('error')
+        }
+    }
+    useEffect(() => {
+        fetchOne()
+    }, [])
     const [nowList, setnowList] = useState(1)
     const inputRef = useRef(null);
     const focusInput = () => {
@@ -16,136 +37,219 @@ const AddEmployee = () => {
     };
     return (
         <>
-
-        <div className='cha'>
-        <div className="addEmployee">
-                <div className="topList">
-                    {nowList == 1 ? < div onClick={() => { setnowList(1) }} className="topDetailOn">
-                        <FaUser />
-                        <p>Personal Information</p>
-                    </div>
-                        :
-                        <div onClick={() => { setnowList(1) }} className="topDetailOff">
+            <div className='cha'>
+                <div className="addEmployee">
+                    <div className="topList">
+                        {nowList == 1 ? < div onClick={() => { setnowList(1) }} className="topDetailOn">
                             <FaUser />
                             <p>Personal Information</p>
                         </div>
-                    }
-                    {nowList == 2 ? < div onClick={() => { setnowList(2) }} className="topDetailOn">
-                        <SlBriefcase />
-                        <p>Professional Information</p>
-                    </div>
-                        :
-                        <div onClick={() => { setnowList(2) }} className="topDetailOff">
+                            :
+                            <div onClick={() => { setnowList(1) }} className="topDetailOff">
+                                <FaUser />
+                                <p>Personal Information</p>
+                            </div>
+                        }
+                        {nowList == 2 ? < div onClick={() => { setnowList(2) }} className="topDetailOn">
                             <SlBriefcase />
                             <p>Professional Information</p>
                         </div>
-                    }
-                    {nowList == 3 ? < div onClick={() => { setnowList(3) }} className="topDetailOn">
-                        <IoDocumentTextOutline />
-
-                        <p>Documents</p>
-                    </div>
-                        :
-                        <div onClick={() => { setnowList(3) }} className="topDetailOff">
+                            :
+                            <div onClick={() => { setnowList(2) }} className="topDetailOff">
+                                <SlBriefcase />
+                                <p>Professional Information</p>
+                            </div>
+                        }
+                        {nowList == 3 ? < div onClick={() => { setnowList(3) }} className="topDetailOn">
                             <IoDocumentTextOutline />
 
                             <p>Documents</p>
                         </div>
-                    }
-                    {nowList == 4 ? < div onClick={() => { setnowList(4) }} className="topDetailOn">
-                        <IoAccessibilityOutline />
+                            :
+                            <div onClick={() => { setnowList(3) }} className="topDetailOff">
+                                <IoDocumentTextOutline />
 
-                        <p>Account Access</p>
-                    </div>
-                        :
-                        <div onClick={() => { setnowList(4) }} className="topDetailOff">
+                                <p>Documents</p>
+                            </div>
+                        }
+                        {nowList == 4 ? < div onClick={() => { setnowList(4) }} className="topDetailOn">
                             <IoAccessibilityOutline />
+
                             <p>Account Access</p>
                         </div>
-                    }
-                </div>
-                {nowList == 1 && <>
-                    <div className="content">
-                        <div style={{ display: 'flex' }}>
-                            <div onClick={focusInput} className="addPhoto">
-                                <IoCameraOutline size={25} />
+                            :
+                            <div onClick={() => { setnowList(4) }} className="topDetailOff">
+                                <IoAccessibilityOutline />
+                                <p>Account Access</p>
                             </div>
-                            <input style={{ display: 'none' }} ref={inputRef} type="file" />
+                        }
+                    </div>
+                    {nowList == 1 && <>
+                        <div className="content">
+                            <div style={{ display: 'flex' }}>
+                                <div onClick={focusInput} className="addPhoto">
+                                    <IoCameraOutline size={25} />
+                                </div>
+                                <input style={{ display: 'none' }} ref={inputRef} type="file" />
+                            </div>
+                            <form className="input2">
+                                <input placeholder='First Name' className="firstName"></input>
+                                <input placeholder='Last Name' className="lastName"></input>
+                                <input placeholder='Mobile Number' className="mobileNumber"></input>
+                                <input placeholder='Email Address' className="emailAddress"></input>
+                                <input placeholder='Date of Birth' className="dateOfBirth"></input>
+                                <input placeholder='Marital Status' className="maritalStatus"></input>
+                                <input placeholder='Gender' className="gender"></input>
+                                <input placeholder='Nationality' className="Nationality"></input>
+                            </form>
+                            <form className="input1">
+                                <input placeholder='Address' className="address" />
+                            </form>
+                            <form className="input3">
+                                <input placeholder='City' className='city'></input>
+                                <input placeholder='State' className="state"></input>
+                                <input placeholder='Zip Code' className='zipCode'></input>
+                            </form>
                         </div>
-                        <form className="input2">
-                            <input placeholder='First Name' className="firstName"></input>
-                            <input placeholder='Last Name' className="lastName"></input>
-                            <input placeholder='Mobile Number' className="mobileNumber"></input>
-                            <input placeholder='Email Address' className="emailAddress"></input>
-                            <input placeholder='Date of Birth' className="dateOfBirth"></input>
-                            <input placeholder='Marital Status' className="maritalStatus"></input>
-                            <input placeholder='Gender' className="gender"></input>
-                            <input placeholder='Nationality' className="Nationality"></input>
-                        </form>
-                        <form className="input1">
-                            <input placeholder='Address' className="address" />
-                        </form>
-                        <form className="input3">
-                            <input placeholder='City' className='city'></input>
-                            <input placeholder='State' className="state"></input>
-                            <input placeholder='Zip Code' className='zipCode'></input>
-                        </form>
-                    </div>
-                    <div className="buttom">
-                        <button className="cancel">Cancel</button>
-                        <button onClick={() => { setnowList(nowList + 1) }} className="next">Next</button>
-                    </div>
-                </>
-                }
-                {nowList == 2 && <>
-                    <div className="content">
-                        <form className="input2">
-                            <input placeholder='Employee ID' className="employeeID"></input>
-                            <input placeholder='User Name' className="userName"></input>
-                            <input placeholder='Employee Type' className="employeeType"></input>
-                            <input placeholder='Email Address' className="emailAddressCompany"></input>
-                            <input placeholder='Department' className="dapartment"></input>
-                            <input placeholder='Enter Designation' className="enterDesighnation"></input>
-                            <input placeholder='Working Days' className="workingDays"></input>
-                            <input placeholder='Joining Date' className="joiningDate"></input>
-                        </form>
-                        <form className="input1">
-                            <input placeholder='Office Location' className="officeLocation" />
-                        </form>
-                    </div>
-                    <div className="buttom">
-                        <button className="cancel">Cancel</button>
-                        <button onClick={() => { setnowList(nowList + 1) }} className="next">Next</button>
-                    </div>
-                </>
-                }
-                {nowList == 3 && <>
-                    <div className="buttom">
-                        <button className="cancel">Cancel</button>
-                        <button onClick={() => { setnowList(nowList + 1) }} className="next">Next</button>
-                    </div>
-                </>
-                }
-                {nowList == 4 && <>
-                    <div className="content">
-                        <form className="input2">
-                            <input placeholder='Enter Email Address' className="firstName"></input>
-                            <input placeholder='Enter Stack ID' className="lastName"></input>
-                            <input placeholder='Enter Skype ID' className="mobileNumber"></input>
-                            <input placeholder='Enter Github ID' className="emailAddress"></input>
-                        </form>
+                        <div className="buttom">
+                            <button style={{
 
-                    </div>
-                    <div className="buttom">
-                        <button className="cancel">Cancel</button>
-                        <button className="add">Add</button>
-                    </div>
-                </>
-                }
+                                width: '250px',
+                                height: '50px',
+                                paddingLeft: '20px',
+                                borderRadius: '10px',
 
-            </div >
-        </div>
-            
+                                fontSize: '16px',
+                                fontWeight: '300',
+                                lineHeight: '24px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                backgroundColor: ' rgba(113, 82, 243, 1)',
+                                color: 'White',
+                                border: 'none',
+
+
+                            }} onClick={() => {
+                                setToogle(true)
+
+                            }}><IoIosAddCircleOutline />Add Employee Test </button>
+                            <button className="cancel">Cancel</button>
+                            <button onClick={() => { setnowList(nowList + 1) }} className="next">Next</button>
+                        </div>
+                    </>
+                    }
+                    {nowList == 2 && <>
+                        <div className="content">
+                            <form className="input2">
+                                <input placeholder='Employee ID' className="employeeID"></input>
+                                <input placeholder='User Name' className="userName"></input>
+                                <input placeholder='Employee Type' className="employeeType"></input>
+                                <input placeholder='Email Address' className="emailAddressCompany"></input>
+                                <input placeholder='Department' className="dapartment"></input>
+                                <input placeholder='Enter Designation' className="enterDesighnation"></input>
+                                <input placeholder='Working Days' className="workingDays"></input>
+                                <input placeholder='Joining Date' className="joiningDate"></input>
+                            </form>
+                            <form className="input1">
+                                <input placeholder='Office Location' className="officeLocation" />
+                            </form>
+                        </div>
+                        <div className="buttom">
+                            <button style={{
+
+                                width: '250px',
+                                height: '50px',
+                                paddingLeft: '20px',
+                                borderRadius: '10px',
+
+                                fontSize: '16px',
+                                fontWeight: '300',
+                                lineHeight: '24px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                backgroundColor: ' rgba(113, 82, 243, 1)',
+                                color: 'White',
+                                border: 'none',
+
+
+                            }} onClick={() => {
+                                setToogle(true)
+
+                            }}><IoIosAddCircleOutline />Add Employee Test </button>
+                            <button className="cancel">Cancel</button>
+                            <button onClick={() => { setnowList(nowList + 1) }} className="next">Next</button>
+                        </div>
+                    </>
+                    }
+                    {nowList == 3 && <>
+                        <div className="buttom">
+                            <button style={{
+
+                                width: '250px',
+                                height: '50px',
+                                paddingLeft: '20px',
+                                borderRadius: '10px',
+
+                                fontSize: '16px',
+                                fontWeight: '300',
+                                lineHeight: '24px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                backgroundColor: ' rgba(113, 82, 243, 1)',
+                                color: 'White',
+                                border: 'none',
+
+
+                            }} onClick={() => {
+                                setToogle(true)
+
+                            }}><IoIosAddCircleOutline />Add Employee Test </button>
+                            <button className="cancel">Cancel</button>
+                            <button onClick={() => { setnowList(nowList + 1) }} className="next">Next</button>
+                        </div>
+                    </>
+                    }
+                    {nowList == 4 && <>
+                        <div className="content">
+                            <form className="input2">
+                                <input placeholder='Enter Email Address' className="firstName"></input>
+                                <input placeholder='Enter Stack ID' className="lastName"></input>
+                                <input placeholder='Enter Skype ID' className="mobileNumber"></input>
+                                <input placeholder='Enter Github ID' className="emailAddress"></input>
+                            </form>
+
+                        </div>
+                        <div className="buttom">
+                            <button style={{
+
+                                width: '250px',
+                                height: '50px',
+                                paddingLeft: '20px',
+                                borderRadius: '10px',
+
+                                fontSize: '16px',
+                                fontWeight: '300',
+                                lineHeight: '24px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                backgroundColor: ' rgba(113, 82, 243, 1)',
+                                color: 'White',
+                                border: 'none',
+
+
+                            }} onClick={() => {
+                                setToogle(true)
+
+                            }}><IoIosAddCircleOutline />Add Employee Test </button>
+                            <button className="cancel">Cancel</button>
+                            <button className="add">Add</button>
+                        </div>
+                    </>
+                    }
+
+                </div >
+            </div>
+            {toogle && arr?.length > 0 && <Modal4 toogle={setToogle} setArr={setArr} arr={arr} />}
         </>
     )
 }
--- src/components/ListFunc/index.jsx
+++ src/components/ListFunc/index.jsx
@@ -58,16 +58,14 @@ export const Button = () => {
                             <p style={{ fontFamily: 'lexend', fontSize: '24px' }}>HRSM</p>
                         </div>
                         <button className={userId === "Dashboard" ? 'selected' : ''} onClick={() => handleCheckbut("Dashboard")}><RxDashboard />Dashboard</button>
-
-
                         <button className={userId === "Allemployee" ? 'selected' : ''} onClick={() => handleCheckbut("Allemployee")}><BsPersonLinesFill />All employees</button>
                         <button className={userId === "AllDepartments" ? 'selected' : ''} onClick={() => handleCheckbut("AllDepartments")}><MdOutlineLocalFireDepartment />All departments</button>
                         <button className={userId === "Attendance" ? 'selected' : ''} onClick={() => handleCheckbut("Attendance")}><GrSchedules />Attendance</button>
                         <button className={userId === "Payroll" ? 'selected' : ''} onClick={() => handleCheckbut("Payroll")}><CiDollar />Payroll</button>
                         <button className={userId === "Jobs" ? 'selected' : ''} onClick={() => handleCheckbut("Jobs")}><RxBackpack />Jobs</button>
                         <button className={userId === "Candidates" ? 'selected' : ''} onClick={() => handleCheckbut("Candidates")}><IoPersonAddOutline />Candidates</button>
-                        <button className={userId === "Dashboard" ? 'selected' : ''} onClick={() => handleCheckbut("Dashboard")}><MdEventNote />Leaves</button>
-                        <button className={userId === "Leave" ? 'selected' : ''} onClick={() => handleCheckbut("Leave")}><LuCalendarDays />Holidays</button>
+                        <button className={userId === "Leave" ? 'selected' : ''} onClick={() => handleCheckbut("Leave")}><MdEventNote />Leaves</button>
+                        <button className={userId === "Holiday" ? 'selected' : ''} onClick={() => handleCheckbut("Holiday")}><LuCalendarDays />Holidays</button>
                         <button className={userId === "Setting" ? 'selected' : ''} onClick={() => handleCheckbut("Setting")}><IoSettingsOutline />Setting</button>
                     </div>
 
