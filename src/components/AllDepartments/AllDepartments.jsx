24971497b1792a52a0c755830216a76786e052be
AnhTrung1997 <trananhtrungtm@gmail.com>
2024-11-23 16:14:39 +0700

    trup

--- src/components/AllDepartments/AllDepartments.jsx
+++ src/components/AllDepartments/AllDepartments.jsx
@@ -1,6 +1,7 @@
 import { CiSearch } from "react-icons/ci";
 import './AllDepartments.css'
 import { useEffect, useState } from "react";
+import { CiBellOn } from "react-icons/ci";
 
 const AllDepartments = () => {
     const [dataEmployee, setdataEmployee] = useState([])
@@ -20,51 +21,93 @@ const AllDepartments = () => {
     })
     return (
         <>
-        <div className="cha">
-        <div className="AllDepartments">
-                <div className="boxSearch">
-                    <CiSearch />
-                    <input className="search" placeholder="Search" type="text" />
+            <div style={{ width: '1230px' }} className="content">
+                <div style={{ marginBottom: '50px', marginTop: '20px', width: '1107px', marginLeft: '65px' }} className='header'>
+                    <div className='header-1'>
+                        <p style={{
+                            fontSize: '20px',
+                            fontWeight: '600',
+                            lineHeight: '30px',
+                            textAlign: 'left'
+                        }}>All Departments</p>
+                        <p style={{
+                            fontSize: '14px',
+                            fontWeight: '300',
+                            lineHeight: '22px',
+                            textAlign: 'left',
+                            color: 'rgba(162, 161, 168, 1)'
+                        }}  >All Departments</p>
+                    </div>
+                    <div className='header-2' style={{
+                        position: 'relative',
+                        height: '50px',
+                        display: 'flex',
+                        gap: '12px'
+                    }}>
+                        <input type="text" style={{
+                            border: '1px solid rgba(162, 161, 168, 0.1)',
+                            paddingLeft: '40px',
+                            height: '50px'
+                        }} />
+                        <label htmlFor="" style={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '20px',
+                            transform: 'translateY(-50%)'
+                        }}><CiSearch size={24} />
+                        </label>
+                        <button style={{
+                            borderRadius: '10px',
+                            backgroundColor: 'rgba(162, 161, 168, 0.1)'
+
+                        }}><CiBellOn size={24} /></button>
+                    </div>
                 </div>
-                <div className="listDepartment">
-                    {listDepart.map((data) => {
+                <div className="AllDepartments">
+                    <div className="boxSearch">
+                        <CiSearch />
+                        <input className="search" placeholder="Search" type="text" />
+                    </div>
+                    <div className="listDepartment">
+                        {listDepart.map((data) => {
 
-                        const memDepart = []
-                        dataEmployee.forEach(item => {
-                            if (item.department == data) {
-                                memDepart.push(item)
-                            }
-                        })
-                        return (
-                            <>
-                                <div className="detailDepartment">
-                                    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }} className="topDepart">
-                                        <h3 style={{ fontSize: '20px' }}>{data}</h3>
-                                        <p style={{ fontSize: '14px', color: '#A2A1A8' }}>{memDepart.length} member</p>
-                                        <hr style={{ width: '100%' }} />
-                                        {memDepart.map((item) => {
-                                            return (
-                                                <div style={{ display: 'flex', gap: '20px' }} className="member">
-                                                    <img style={{ height: '40px', width: '40px', borderRadius: '50%' }} src={item.img} />
-                                                    <div>
-                                                        <p>{item.name}</p>
-                                                        <p style={{ fontSize: '12px', color: '#A2A1A8' }}>{item.designation}</p>
-                                                    </div>
+                            const memDepart = []
+                            dataEmployee.forEach(item => {
+                                if (item.department == data) {
+                                    memDepart.push(item)
+                                }
+                            })
+                            return (
+                                <>
+                                    <div className="content">
 
-                                                </div>
-                                            )
-                                        })}
+                                        <div className="detailDepartment">
+                                            <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }} className="topDepart">
+                                                <h3 style={{ fontSize: '20px' }}>{data}</h3>
+                                                <p style={{ fontSize: '14px', color: '#A2A1A8' }}>{memDepart.length} member</p>
+                                                <hr style={{ width: '100%' }} />
+                                                {memDepart.map((item) => {
+                                                    return (
+                                                        <div style={{ display: 'flex', gap: '20px' }} className="member">
+                                                            <img style={{ height: '40px', width: '40px', borderRadius: '50%' }} src={item.img} />
+                                                            <div>
+                                                                <p>{item.name}</p>
+                                                                <p style={{ fontSize: '12px', color: '#A2A1A8' }}>{item.designation}</p>
+                                                            </div>
 
+                                                        </div>
+                                                    )
+                                                })}
+
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </>
-                        )
-                    })}
+                                </>
+                            )
+                        })}
+                    </div >
                 </div>
-
             </div>
-        </div>
-            
         </>
     )
 }
--- src/components/Candidates/Candidates.jsx
+++ src/components/Candidates/Candidates.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { CiSearch } from "react-icons/ci"
+import { CiBellOn } from "react-icons/ci";
 import './Candidates.css'
 const Candidates = () => {
 
@@ -15,45 +16,88 @@ const Candidates = () => {
 
     return (
         <>
-            <div className="main">
-                <div className="boxSearch">
-                    <CiSearch />
-                    <input className="search" placeholder="Search" type="text" />
-                </div>
-                <div className="Candidates">
-                    <div style={{ color: '#A2A1A8' }} className="content">
-                        <div className="checkbox">
-                            <input type="checkbox" />
-                        </div>
-                        <div className="candi">Candidates</div>
-                        <div className="appFor">Applied For</div>
-                        <div className="appDate">Applied Date</div>
-                        <div className="email">Email Address</div>
-                        <div className="mobile">Moblie Number</div>
-                        <div className="status">Status</div>
+            <div className="content">
+                <div style={{ marginBottom: '50px', marginTop: '20px' }} className='header'>
+                    <div className='header-1'>
+                        <p style={{
+                            fontSize: '20px',
+                            fontWeight: '600',
+                            lineHeight: '30px',
+                            textAlign: 'left'
+                        }}>Candidates</p>
+                        <p style={{
+                            fontSize: '14px',
+                            fontWeight: '300',
+                            lineHeight: '22px',
+                            textAlign: 'left',
+                            color: 'rgba(162, 161, 168, 1)'
+                        }}  >All employees Candidates</p>
                     </div>
-                    {dataCandidates.map(item => {
-                        return (
-                            <div className="content">
-                                <div className="checkbox">
-                                    <input type="checkbox" />
-                                </div>
-                                <div className="candi">
-                                    <img style={{ height: '36px', width: '36px', borderRadius: '50%' }} src={item.img} />
-                                    <p>{item.Name}</p>
-                                </div>
-                                <div className="appFor">{item.Designation}</div>
-                                <div className="appDate">{item.ApplieDate}</div>
-                                <div className="email">{item.Email}</div>
-                                <div className="mobile">{item.Moblie}</div>
-                                <div className="status">
-                                    <div className={item.Status}>{item.Status}</div>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
+                    <div className='header-2' style={{
+                        position: 'relative',
+                        height: '50px',
+                        display: 'flex',
+                        gap: '12px'
+                    }}>
+                        <input type="text" style={{
+                            border: '1px solid rgba(162, 161, 168, 0.1)',
+                            paddingLeft: '40px',
+                            height: '50px'
+                        }} />
+                        <label htmlFor="" style={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '20px',
+                            transform: 'translateY(-50%)'
+                        }}><CiSearch size={24} />
+                        </label>
+                        <button style={{
+                            borderRadius: '10px',
+                            backgroundColor: 'rgba(162, 161, 168, 0.1)'
 
+                        }}><CiBellOn size={24} /></button>
+                    </div>
+                </div>
+                <div className="main">
+                    <div className="boxSearch">
+                        <CiSearch />
+                        <input className="search" placeholder="Search" type="text" />
+                    </div>
+                    <div className="Candidates">
+                        <div style={{ color: '#A2A1A8' }} className="content">
+                            <div className="checkbox">
+                                <input type="checkbox" />
+                            </div>
+                            <div className="candi">Candidates</div>
+                            <div className="appFor">Applied For</div>
+                            <div className="appDate">Applied Date</div>
+                            <div className="email">Email Address</div>
+                            <div className="mobile">Moblie Number</div>
+                            <div className="status">Status</div>
+                        </div>
+                        {dataCandidates.map(item => {
+                            return (
+                                <div className="content">
+                                    <div className="checkbox">
+                                        <input type="checkbox" />
+                                    </div>
+                                    <div className="candi">
+                                        <img style={{ height: '36px', width: '36px', borderRadius: '50%' }} src={item.img} />
+                                        <p>{item.Name}</p>
+                                    </div>
+                                    <div className="appFor">{item.Designation}</div>
+                                    <div className="appDate">{item.ApplieDate}</div>
+                                    <div className="email">{item.Email}</div>
+                                    <div className="mobile">{item.Moblie}</div>
+                                    <div className="status">
+                                        <div className={item.Status}>{item.Status}</div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+
+                </div>
             </div>
         </>
     )
